dd9ab8f26ed0191c50f0b36130157c20970fb99b
joq62 <jakim.leche@gmail.com>
2023-05-03 01:03:45 +0200

    ud

--- src/lib_provider.erl
+++ src/lib_provider.erl
@@ -116,8 +116,7 @@ load(ProviderSpec,HostSpec)->
 									       false->
 										  {error,["Not loaded  ",App,?MODULE,?FUNCTION_NAME,?LINE]};
 									       true->
-										   ok
-										 %  {ok,ProviderNode,App}
+										   {ok,ProviderSpec,HostSpec,ProviderNode,App}
 									   end
 								   end
 							   end
